558cc226831fea73f07a13a037b6284918e11c31
Joseph Ahrendsen <josephahrendsen@gmail.com>
2021-08-12 12:19:35 -0400

    added form code

--- app/views.py
+++ app/views.py
@@ -1,17 +1,39 @@
 from django.shortcuts import render
+from app.forms import LoginForm
 import requests
 
 f = open("api_key.txt", "r")
 api_key = f.read()
 
-# Create your views here.
+
 def index(request):
+    data = {}
+    return render(request, 'app/index.html', data)
+
+
+def index_post(request):
+    if request.method == "POST":
+        # Get the posted form
+        MyLoginForm = LoginForm(request.POST)
+
+        if MyLoginForm.is_valid():
+            cuisine = MyLoginForm.cleaned_data['username']
+            distance = MyLoginForm.cleaned_data['distance']
+            price = MyLoginForm.cleaned_data['price']
+            rating = MyLoginForm.cleaned_data['rating']
+    else:
+        MyLoginForm = LoginForm()
+
     headers = {'Authorization': 'bearer ' + api_key}
-    response = requests.get('https://api.yelp.com/v3/businesses/good-food-on-montford-charlotte-2', headers=headers)
+    query = 0
+
+    response = requests.get(
+        query, headers=headers
+    )
     data = {'response': response.json()}
     return render(request, 'app/index.html', data)
+
 
 def dashboard(request):
     data = {}
     return render(request, 'app/dashboard.html', data)
-
