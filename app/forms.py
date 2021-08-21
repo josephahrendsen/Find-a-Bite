c726a790210d252573e0d208463b0a8edaf14da9
Joseph Ahrendsen <josephahrendsen@gmail.com>
2021-08-21 16:33:11 -0400

    css fixes

--- app/forms.py
+++ app/forms.py
@@ -12,10 +12,10 @@ class QueryForm(forms.Form):
       ("italian", "Italian"),
    )
    PRICE_CHOICES = (
-      (1, "$"),
-      (2, "$$"),
-      (3, "$$$"),
-      (4, "$$$$"),
+      (1, "$ (0-10)"),
+      (2, "$$ (11-30)"),
+      (3, "$$$ (31-60)"),
+      (4, "$$$$ (61+)"),
    )
    categories = forms.MultipleChoiceField(label="Cuisine", choices=CAT_CHOICES, widget=forms.CheckboxSelectMultiple(attrs={'class': 'form-control'}))
    location = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
